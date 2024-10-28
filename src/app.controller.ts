a908262dba0899d6e6817d8974b9bb32e6935cb1
Soubhik Gon <119437069+zakhaev26@users.noreply.github.com>
2024-10-28 10:18:56 +0530

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,10 @@
-import { BadRequestException, Body, Controller, Get, Post, Req, Res, UnauthorizedException } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import * as bcrypt from 'bcrypt';
-import { JwtService } from "@nestjs/jwt";
-import { Response, Request } from 'express';
 
-@Controller('api')
+@Controller()
 export class AppController {
     constructor(
         private readonly appService: AppService,
-        private jwtService: JwtService
     ) {}
 
   @Public()
