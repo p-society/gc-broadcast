475733a70d124b57412936ec04fc97d52872d6f0
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2024-12-16 12:37:02 +0530

    chore(global-service): fix `option` passing

--- src/common/options.ts
+++ src/common/options.ts
@@ -3,7 +3,10 @@
  */
 
 export default {
+  handleSoftDelete: true,
   deleteKey: 'deleted',
+  deletedByKey: 'deletedBy',
+  deletedAtKey: 'deletedAt',
   defaultPagination: true,
   defaultLimit: 20,
   defaultSkip: 0,
