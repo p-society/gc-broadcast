426386aba2e2f535e821ad4fd78865c8b7a3fff0
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2024-12-21 03:13:31 +0530

    fix: remove unnecessary deps

--- src/services/bullmq/processors/reaction-stream.processor.ts
+++ src/services/bullmq/processors/reaction-stream.processor.ts
@@ -1,10 +1,8 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { REACTION_STREAM_QUEUE } from '../constants/queues';
 import { Job } from 'bullmq';
-import { PresenceGateway } from 'src/services/gateways/presence/presence.gateway';
 import { ReactionStreamJob } from '../jobs/reactions.job';
 import { ReactionSocketEvents } from 'src/services/gateways/constants/reaction.events';
-import { ReactionGateway } from 'src/services/gateways/reactions/reactions.gateway';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Processor(REACTION_STREAM_QUEUE)
