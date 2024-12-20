ceb6a3366758c9380c98824e35f92ec66262e4fe
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2024-12-20 20:16:57 +0530

    feat: kafka v2, adds releaser

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PRIVATE_IP=$(ifconfig wlp2s0 | grep 'inet ' | awk '{print $2}')
+if [ -z "$PRIVATE_IP" ]; then
+  echo "Private IP not found!"
+  exit 1
+fi
+
+export PRIVATE_IP
+sudo docker stop $(sudo docker ps -aq) && sudo docker rm $(sudo docker ps -aq)
+sudo docker-compose -f docker/docker-compose.dev.yml up -d
